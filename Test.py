7d021bab7bcf99dff2a637aa6e378684df1641ec
ViratTomar <xylova24@gmail.com>
2024-12-28 14:00:21 +0530

    Initial commit

---
+++ Test.py
@@ -0,0 +1,12 @@
+name = input("What is your Name? ")
+print ("Hello " + name)
+birth_year = input("What is your Birth Year? ")
+age = 2024 - int(birth_year)
+print ("I calculated that your age is " + str(age))
+print ("OK, Let's keep moving")
+print ("Please give me some numbers to Add as I like to do so.")
+number_a = int(input("What will be the First Number to Add? "))
+number_b = int(input("What will be the Second Number to Add? "))
+total = number_a + number_b
+print(f"The total of {number_a} and {number_b} is {total}")
+print ("Thankyou so much " + name)
